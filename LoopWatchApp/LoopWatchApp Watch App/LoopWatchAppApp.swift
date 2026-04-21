b23d4093cacd750da0c73c8e9ee78f24fdb4e66a
Carl Christensen <95908319+threecee-adsk@users.noreply.github.com>
2026-04-21 20:28:39 +0200

    Phase 1: scaffold LoopWatchApp watchOS project with entitlements, Info.plist, linked frameworks

---
+++ LoopWatchApp/LoopWatchApp Watch App/LoopWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopWatchAppApp.swift
+//  LoopWatchApp Watch App
+//
+//  Created by Carl Christensen on 21/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct LoopWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
